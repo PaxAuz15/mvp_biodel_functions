45bde7f1729d2c7c024f8b9bb8208bc0f32f9187
Luis Auz <luisauz.geek@gmail.com>
2021-07-01 08:26:42 -0500

    feat: get token & save user & validate handle

--- functions/index.js
+++ functions/index.js
@@ -86,6 +86,7 @@ app.post('/signup',(req,res)=>{
         handle: req.body.handle,
     };
 
+    let token, userId;
     db.doc(`/users/${newUser.handle}`).get()
         .then(doc => {
             if(doc.exists){
@@ -96,11 +97,32 @@ app.post('/signup',(req,res)=>{
                     .createUserWithEmailAndPassword(newUser.email,newUser.password);
             }
         })
-        .then(data => data.user.getIdToken())
-        .then(token => res.status(201).json({ token }))
+        .then(data =>{
+            userId = data.user.uid;
+            return data.user.getIdToken();
+        })
+        .then(idToken =>{
+            token = idToken;
+            const userCredentials = {
+                handle: newUser.handle,
+                email: newUser.email,
+                createdAt: new Date().toISOString(),
+                userId
+            };
+            try{
+                return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+            } catch{
+                console.error('ERROR DE GUARDAR')
+            }
+        })
+        .then(()=> res.status(201).json({ token }))
         .catch(err => {
             console.error(err);
-            return res.status(500).json({ error: err.code });
+            if (err.code === 'auth/email-already-in-use'){
+                return res.status(400).json({ email: 'Email is already in use' })
+            }else{
+                return res.status(500).json({ error: err.code });
+            }
         })
 })
 
